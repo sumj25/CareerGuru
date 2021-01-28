a2da09e7ef1d27f8ecf4f3d15f85fec437b75ae0
Suman kumar <59159432+sumj25@users.noreply.github.com>
2021-01-28 18:42:20 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
 export default {
    firebaseConfig : {
-        apiKey: "AIzaSyDvzyFRlhcEZx6qI_JElAEqYGlcoaCttAI",
-        authDomain: "jobt-bf8c0.firebaseapp.com",
-        projectId: "jobt-bf8c0",
-        storageBucket: "jobt-bf8c0.appspot.com",
-        messagingSenderId: "425748663369",
-        appId: "1:425748663369:web:5a9a01a11c0abc84874398"
+        apiKey: "xxxxxx",
+        authDomain: "xxxxx",
+        projectId: "xxxx",
+        storageBucket: "xxxxx",
+        messagingSenderId: "xxx",
+        appId: "xxxxx"
       }
 }
